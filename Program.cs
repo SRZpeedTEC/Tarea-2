7dc2205a18b6a7dae77a4f5e5bfdbc75a37e1be6
SRZpeedTEC <s.boza.1@estudiantec.cr>
2024-09-04 18:23:31 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Tarea_2
+{
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {         
+
+        }
+
+        public static string Prueba()
+        {
+            return "Hola Mundo";
+        }
+        
+
+
+    }
+}
